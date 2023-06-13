8c77247111aac3e3936cc0ff2ac98f056d408652
dhruv.g@gurukul.org <dhruv.g@gurukul.org>
2023-06-13 11:16:13 +0530

    Stock-Management remove space

--- src/app/stock-item/stock-item.component.ts
+++ src/app/stock-item/stock-item.component.ts
@@ -8,7 +8,7 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 export class StockItemComponent {
 
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'Qulity', 'selling', 'Remaix', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'weight', 'Qulity', 'selling', 'Remain', 'symbol'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
   // @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -24,18 +24,17 @@ export interface PeriodicElement {
   weight: number;
   Qulity: number;
   selling: number;
-  Remaix: number;
+  Remain: number;
   symbol: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, Qulity: 20, selling: 23, Remaix: 43, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, Qulity: 20, selling: 23, Remaix: 43, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, Qulity: 20, selling: 23, Remaix: 43, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, Qulity: 20, selling: 23, Remaix: 43, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, Qulity: 20, selling: 23, Remaix: 43, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, Qulity: 20, selling: 23, Remaix: 43, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, Qulity: 20, selling: 23, Remaix: 43, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, Qulity: 20, selling: 23, Remaix: 43, symbol: 'O' },
-
+  { position: 1, name: 'Hydrogen', weight: 1.0079, Qulity: 20, selling: 23, Remain: 43, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, Qulity: 20, selling: 23, Remain: 43, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, Qulity: 20, selling: 23, Remain: 43, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, Qulity: 20, selling: 23, Remain: 43, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, Qulity: 20, selling: 23, Remain: 43, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, Qulity: 20, selling: 23, Remain: 43, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, Qulity: 20, selling: 23, Remain: 43, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, Qulity: 20, selling: 23, Remain: 43, symbol: 'O' },
 ];
